d81b1cb608ca612a5365bdd9cb69399cae666720
ncasanovas <nataliasolcasanovas@gmail.com>
2019-12-13 16:06:15 -0300

    First commit

---
+++ src/components/Tetris.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {createStage} from '../gameHelpers';
+import Stage from './Stage';
+import Display from './Display';
+import StartButton from './StartButton';
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
+
+const Tetris = () => {
+
+    return(
+        <StyledTetrisWrapper>
+            <StyledTetris>
+                <Stage stage={createStage()} />
+                <aside>
+                    <div>
+                        <Display text="Score" />
+                        <Display text="Rows" />
+                        <Display text="Level" />
+                    </div>
+                    <StartButton />
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
+    )
+}
+
+export default Tetris;
